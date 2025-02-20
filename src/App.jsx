a5679205c6fd24222b1b6198c90ba6e90ca437e6
mikereyes102 <mikereyes102@gmail.com>
2025-02-20 09:44:20 -0500

    Set up Firebase and created sign in component. Added sign in routing to the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
